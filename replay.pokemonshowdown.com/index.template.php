e949fef4ef8a55a420f41a860a7ca23e777dab86
florian <tectoast@gmail.com>
2024-09-25 17:44:01 +0200

    refactor: remove ads from replay

--- replay.pokemonshowdown.com/index.template.php
+++ replay.pokemonshowdown.com/index.template.php
@@ -110,20 +110,6 @@ https://replay.pokemonshowdown.com/gen7randomdoublesbattle-865046831.log
 <link rel="stylesheet" href="//play.pokemonshowdown.com/style/battle.css?a7" />
 <link rel="stylesheet" href="//play.pokemonshowdown.com/style/utilichart.css?a7" />
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26211653-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-26211653-1');
-</script>
-<!-- End Google Analytics -->
-<!-- Venatus Ad Manager - Install in <HEAD> of page -->
-	<script src="https://hb.vntsm.com/v3/live/ad-manager.min.js" type="text/javascript" data-site-id="642aba63ec9a7b11c3c9c1be" data-mode="scan" async></script>
-<!-- / Venatus Ad Manager -->
-
 <style>
 	@media (max-width:820px) {
 		.battle {
